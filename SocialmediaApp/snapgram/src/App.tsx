8457ac260b77438dcdb578e7aff43d1192ff67b1
Vasant19 <vasantdave2002@gmail.com>
2024-11-05 17:25:39 -0500

    Modified Configuration files for tailwind and shadcn ,configured json packages,  added and implemented button component

--- SocialmediaApp/snapgram/src/App.tsx
+++ SocialmediaApp/snapgram/src/App.tsx
@@ -1,10 +1,28 @@
 import './globals.css' ;
+
+import SignupForm from './_auth/forms/SignupForm';
+import SigninForm from './_auth/forms/SigninForm';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './_root/pages';
+import RootLayout from './_root/RootLayout';
+import AuthLayout from './_auth/AuthLayout';
+
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello Snapgram!
-    </h1></div>
+    <main className='flex h-screen'>
+    <Routes>
+    {/* Public Routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+      </Route>
+
+      {/*Private Routes */}
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+    </main>
   )
 }   
 
